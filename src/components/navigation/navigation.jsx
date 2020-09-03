3a563eb41ce30985ea06e6dd895d0de6f38d8262
Austin Keener <austingraphicandweb@gmail.com>
2020-09-03 13:52:15 -0400

    building out the navigation bar

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import './navigation.css';
+import {Link} from 'react-router-dom';
 
 const navigation = () => {
 
     return (
         <ul>
-            <li>Home</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-            <li>Resume</li>
-            <li>Portfolio</li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/portfolio'>Portfolio</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+            <li><Link to='' target='_blank'>Design</Link></li>
+            <li><Link to='' target='_blank'>Resume</Link></li>
         </ul>
     )
 
