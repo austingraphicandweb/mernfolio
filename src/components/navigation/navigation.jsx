d253ef384d4e03f10e685748adfbed2a7a8ca2a6
Austin Keener <austingraphicandweb@gmail.com>
2020-09-03 16:59:35 -0400

    navigation progress

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -5,13 +5,16 @@ import {Link} from 'react-router-dom';
 const navigation = () => {
 
     return (
+        <div className="navigation">
         <ul>
+            <li><Link to='/' target='_blank' className='logo'><img src=""/><span>Austin Keener</span></Link></li>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/portfolio'>Portfolio</Link></li>
             <li><Link to='/contact'>Contact</Link></li>
             <li><Link to='' target='_blank'>Design</Link></li>
             <li><Link to='' target='_blank'>Resume</Link></li>
         </ul>
+        </div>
     )
 
 }
