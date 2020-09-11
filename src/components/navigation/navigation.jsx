6f21a25029f965d80cd202380516cdec37e14416
Austin Keener <austingraphicandweb@gmail.com>
2020-09-11 18:20:18 -0400

    navigation work

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './navigation.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const navigation = () => {
 
     return (
         <div className="navigation">
-        <ul>
-            <li><Link to='/' target='_blank' className='logo'><img src=""/><span>Austin Keener</span></Link></li>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/portfolio'>Portfolio</Link></li>
-            <li><Link to='/contact'>Contact</Link></li>
-            <li><Link to='' target='_blank'>Design</Link></li>
-            <li><Link to='' target='_blank'>Resume</Link></li>
-        </ul>
+            <h1 className="logo">Austin Keener</h1>
+            <ul className="nav">
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/portfolio'>Portfolio</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+                <li><Link to='' target='_blank'>Design</Link></li>
+                <li><Link to='' target='_blank' className="nodisplay">Resume</Link></li>
+            </ul>
         </div>
     )
 
