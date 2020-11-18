e62b1afd394636df45d1e39f36cb2daa2d86a44a
Austin Keener <austingraphicandweb@gmail.com>
2020-11-18 11:58:12 -0500

    fixing nav and content issue

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import "../home/home.css";
 
 const home = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <main className="homeContainer">
+      <div>
+         Homepage placeholder 
+      </div>
+    </main>
+  );
+};
 
 export default home;
